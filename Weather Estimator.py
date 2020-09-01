bfc62077637a78831669e26873f5ae6d88c47b6d
rahulvishwakarm <48653985+rahulvishwakarm@users.noreply.github.com>
2020-09-01 23:50:31 +0530

    Update Weather Estimator.py

--- Weather Estimator.py
+++ Weather Estimator.py
@@ -1,10 +1,11 @@
+#importing required modules
 import requests
 from bs4 import BeautifulSoup
 print("Weather temperature")
-search=input("Enter the Place:")
+search=input("Enter the Place:") #user input place
 search_new="Weather of "+search
 url=f"https://www.google.com/search?&q={search_new}"
 r=requests.get(url)
-s=BeautifulSoup(r.text,"html.parser")
-update=s.find("div",class_="BNeawe")
-print(search," is ",update.text)
+s=BeautifulSoup(r.text,"html.parser") #creating soup of the search page
+update=s.find("div",class_="BNeawe") 
+print(search," is ",update.text) #geting text from parsed soup
